cba5b2d1eb4959f2f37e2eb2bbbfb6a86e838108
Adrien <adrienducourthial@gmail.com>
2023-10-16 17:06:48 +0200

    prod fix

--- webapp/main.py
+++ webapp/main.py
@@ -6,7 +6,7 @@ import os
 import dataikuapi
 from pathlib import Path
 import streamlit as st
-
+import pandas as pd
 
 client = dataikuapi.APINodeClient("https://api-56ce4d5f-779f448b-dku.eu-west-3.app.dataiku.io/", "AdriPI")
 load_dotenv()
@@ -23,24 +23,39 @@ running = True
 
 topics = [TOPIC]
 
-typess = {'age': 'int', 'class of worker': 'str', 'industry code': 'int', 'occupation code': 'int', 'education': 'str', 'wage per hour': 'int', 'enrolled in edu inst last wk': 'str', 'marital status': 'str', 'major industry code': 'str', 'major occupation code': 'str', 'race': 'str', 'hispanic Origin': 'str', 'sex': 'str', 'member of a labor union': 'str', 'reason for unemployment': 'str', 'full or part time employment stat': 'str', 'capital gains': 'int', 'capital losses': 'int', 'divdends from stocks': 'int', 'tax filer status': 'str', 'region of previous residence': 'str', 'state of previous residence': 'str', 'detailed household and family stat': 'str', 'detailed household summary in household': 'str', 'instance weight': 'float', 'migration code-change in msa': 'str', 'migration code-change in reg': 'str', 'migration code-move within reg': 'str', 'live in this house 1 year ago': 'str', 'migration prev res in sunbelt': 'str', 'num persons worked for employer': 'int', 'family members under 18': 'str', 'country of birth father': 'str', 'country of birth mother': 'str', 'country of birth self': 'str', 'citizenship': 'str', 'own business or self employed': 'int', "fill inc questionnaire for veteran's admin": 'str', 'veterans benefits': 'int', 'weeks worked in year': 'int', 'year': 'int', 'income level': 'str'}
+typess = {'age': 'int', 'class of worker': 'str', 'industry code': 'int', 'occupation code': 'int', 'education': 'str',
+          'wage per hour': 'int', 'enrolled in edu inst last wk': 'str', 'marital status': 'str',
+          'major industry code': 'str', 'major occupation code': 'str', 'race': 'str', 'hispanic Origin': 'str',
+          'sex': 'str', 'member of a labor union': 'str', 'reason for unemployment': 'str',
+          'full or part time employment stat': 'str', 'capital gains': 'int', 'capital losses': 'int',
+          'divdends from stocks': 'int', 'tax filer status': 'str', 'region of previous residence': 'str',
+          'state of previous residence': 'str', 'detailed household and family stat': 'str',
+          'detailed household summary in household': 'str', 'instance weight': 'float',
+          'migration code-change in msa': 'str', 'migration code-change in reg': 'str',
+          'migration code-move within reg': 'str', 'live in this house 1 year ago': 'str',
+          'migration prev res in sunbelt': 'str', 'num persons worked for employer': 'int',
+          'family members under 18': 'str', 'country of birth father': 'str', 'country of birth mother': 'str',
+          'country of birth self': 'str', 'citizenship': 'str', 'own business or self employed': 'int',
+          "fill inc questionnaire for veteran's admin": 'str', 'veterans benefits': 'int',
+          'weeks worked in year': 'int', 'year': 'int', 'income level': 'str'}
 
 
 def nulachier(values):
     req: dict = json.loads(values.decode('utf8'))
+    df = pd.DataFrame(req, index=[0])
     expected = req.pop("income level")
     time_stamp = req.pop("timestamp")
     print(time_stamp)
     print(req)
     temp = {i: eval(typess[i])(j) for i, j in req.items()}
     prediction = client.predict_record("model_classifier", temp)
+    df["prediction"] = prediction["result"]["prediction"]
+    df["good_prediction"] = df["prediction"] == expected
     ret = prediction["result"]
     print(f"expected: {expected}, got: {ret['prediction']}")
-    return ret,expected
+    return df
 
-
-
-
+dff = None
 try:
     consumer.subscribe(topics)
 
@@ -59,9 +74,14 @@ try:
         else:
 
             print("doing int")
+
             res = nulachier(msg.value())
-            st.write(res[0]['prediction'])
-            st.write(res[1])
+            if dff is None:
+                dff = res
+            else:
+                dff = pd.concat([dff, res])
+            st.write(dff)
+
 
 finally:
     # Close down consumer to commit final offsets.
